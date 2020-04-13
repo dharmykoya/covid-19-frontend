9651db9f3a5fcdcd4bb61fa8d5333b3d91b62181
Damilola Adekoya <damilolaadekoya@Damilolas-MacBook-Pro.local>
2020-04-13 13:25:24 +0100

    Updates

---
+++ precache-manifest.1c8b7d3ddd97b0fb8f726eb2a8bcc8b7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7683961a419d5b58303487855032ed3",
+    "url": "/covid-19-frontend/index.html"
+  },
+  {
+    "revision": "2d62d81bf1651cc16321",
+    "url": "/covid-19-frontend/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "e761f9845fe3d90eef8b",
+    "url": "/covid-19-frontend/static/css/main.27f0c9dc.chunk.css"
+  },
+  {
+    "revision": "2d62d81bf1651cc16321",
+    "url": "/covid-19-frontend/static/js/2.fa203c6f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid-19-frontend/static/js/2.fa203c6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e761f9845fe3d90eef8b",
+    "url": "/covid-19-frontend/static/js/main.b3ec3714.chunk.js"
+  },
+  {
+    "revision": "5bc114f3f28f4459781f",
+    "url": "/covid-19-frontend/static/js/runtime-main.c771dfb2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/covid-19-frontend/static/media/logo.5d5d9eef.svg"
+  }
+]);
